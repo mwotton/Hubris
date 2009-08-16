c7bcdbf33fe5bc598aa2f0ae35288d4d2011db3e
Mark Wotton <mwotton@110.20.20.218.optusnet.com.au>
2009-08-16 18:03:43 +1000

    work around double problem, inching closer to full compile

--- lib/hubris.rb
+++ lib/hubris.rb
@@ -10,32 +10,48 @@ module Hubris
       build_jhc(haskell_str)
     end
     
+    def noisy(str)
+      print "Noisy: #{str}\n"
+      system(str)
+    end
     
     def build_jhc(haskell_str)
       file=Tempfile.new("TempHs.hs")
-      # TODO add foreign export calls immediately for each toplevel func
-      # cheap hacky way: first word on each line, nub it to get rid of
-      # function types.
-      # tricky bit: generating interface for each
-      headers = haskell_str.each_line do |line|
-        file.write "#{line.split(/ /)[0]} :: RValue -> IO RValue"
-      end
       # cheap way: assert type sigs binding to RValue. Might be able to do better after,
       # but this'll do for the moment
-      file.write(<<EOF
+      file.print(<<EOF
+import RubyMap
+import Mapper
 main :: IO ()
 main = return ()
 EOF
                  )
-      file.write(haskell_str)
-      if(!system("jhc --no-main #{file.path}")) then
+      file.print(haskell_str)
+      # TODO add foreign export calls immediately for each toplevel func
+      # cheap hacky way: first word on each line, nub it to get rid of
+      # function types.
+      # tricky bit: generating interface for each
+      functions={}
+      haskell_str.each_line do |line|
+        # if line ~= /^[^ ]/ then
+        functions[line.split(/ /)[0]]=1
+      end
+      functions.keys.each do |fname|
+        file.print "#{fname} :: RValue -> IO RValue\n"
+        # end
+      end
+      
+      file.flush
+      # this is so dumb
+      system("cat #{file.path}; cp #{file.path} #{file.path}.hs")
+      if(0!=noisy("jhc  #{file.path}.hs -ilib")) then
           raise SyntaxError, "JHC build failed"
       end
       # output goes to hs_out.code.c
       # don't need to grep out main any more
       # FIXME unique name for dynamic lib
       lib = Tempfile.new("libDyn.so")
-      if(!system("gcc '-std=gnu99' -D_GNU_SOURCE '-falign-functions=4' -ffast-math -Wshadow -Wextra -Wall -Wno-unused-parameter -o libdynhs.so \
+      if(0!=noisy("gcc '-std=gnu99' -D_GNU_SOURCE -D'-falign-functions=4' '-D_JHC_STANDALONE=0' -ffast-math -Wshadow -Wextra -Wall -Wno-unused-parameter -o libdynhs.so \
  -DNDEBUG -D_JHC_STANDALONE=0 -O3 -fPIC -shared #{file.dirname}/hs.out_code.c -o {lib.name}")) then
           raise SyntaxError, "C build failed"
       end
