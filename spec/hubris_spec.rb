7c78b042698c15ff3d0c7184c36244a0b4e58fe0
Mark Wotton <mwotton@114.72.233.215.optusnet.com.au>
2009-08-16 19:52:27 +1000

    fix the tests

--- spec/hubris_spec.rb
+++ spec/hubris_spec.rb
@@ -4,24 +4,18 @@ require File.dirname(__FILE__) + '/spec_helper.rb'
 # http://rspec.info/
 describe "Hubris" do
   
-  it "can build the sample app" do
-    # violated "Be sure to write your specs"
-  end
-  
   it "can whine like a little baby when you pass it bad haskell" do
-    lambda {Hubris::Hubris.new("broken _ = return (T_FIXNUM (1 + \"a string\")")}.should raise_error(SyntaxError)
+    lambda {Hubris::Hubris.new("broken _ = return (1 + \"a string\")")}.should raise_error(SyntaxError)
   end
   
   it "can sing like a golden bird when you treat it right, aw yeah" do
-    lambda {Hubris::Hubris.new("working _ = return (T_FIXNUM (1 + 2))")}.should_not raise_error
+    lambda {Hubris::Hubris.new("working _ = return (toRuby $ 1 + 2)")}.should_not raise_error
   end
   
   it "can double an int in Haskell-land" do
     haskell = Hubris::Hubris.new(<<EOF
 -- partial function, will probably crash and burn
-double (T_FLOAT d)  = return $ T_FLOAT (x+x)
-double (T_FIXNUM i) = return $ T_FIXNUM (x+x)
-double (T_BIGNUM i) = return $ T_BIGNUM (x+x)
+double i = let j = fromRuby i in return (toRuby $ j + j)
 EOF
                                  )
     haskell.double(1).should eql(2)
