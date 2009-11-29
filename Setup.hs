a458aa996c474caf00bc9034b0e0f58f99195232
Mark <mwotton@gmail.com>
2009-11-29 23:45:46 +1100

    filthy hack to get include info into the program

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,17 @@
+{-# LANGUAGE NamedFieldPuns #-}
 import Distribution.Simple
-main = defaultMain
+import Distribution.Simple.Setup
+import Distribution.Simple.LocalBuildInfo
+import Distribution.Simple.Utils
+import Distribution.PackageDescription
+import Distribution.Verbosity
+
+main = defaultMainWithHooks hooks
+
+hooks = simpleUserHooks
+  {
+   preConf = \arg flags -> do
+              -- let ConfigFlags{scratch} = undefined
+      writeFile "includes.hs" ("module Includes where\nextraIncludeDirs=" ++ show (configExtraIncludeDirs flags))
+      return emptyHookedBuildInfo
+  }
