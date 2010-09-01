872dc4d74a09873a4189351dd6db784487eb4b37
Mark Wotton <mwotton@gmail.com>
2010-09-01 11:29:07 +1000

    clean up old examples & scripts, add rvmrc

--- examples/simple_inline/clean_and_run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-# Delete existing binaries to see that the lib correctly recreates them 
-clear; 
-rm -rf  ~/.hubris_cache/*; rm -rf *.o; rm -rf *.hi;  rm -rf *.c;  
-ruby haskell_math.rb 
