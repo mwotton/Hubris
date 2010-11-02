8225b3a46764fc75b83a730f6a392c8e40268399
hudson build agent on ci.shimweasel.com <mwotton@gmail.com>
2010-11-03 10:27:48 +1100

    more build script futzing

--- script/ci.sh
+++ script/ci.sh
@@ -1,15 +1,16 @@
 #!/bin/bash -x
 ghc_version=$1
 set -e
-source $HOME/.rvm/scripts/rvm
+#source $HOME/.rvm/scripts/rvm
 export PATH=$PATH:$HOME/.cabal/bin
 # another huge hack. let's fix this properly soon TODO
 export LD_LIBRARY_PATH=$HOME/.rvm/rubies/ruby-1.9.1-p378/lib/    
 
-# reinstall haskell stuff
-ghc-pkg-$ghc_version unregister hubris || true
 # rm `which Hubrify`
 cd Haskell
+cabal install --with-ghc=ghc-$ghc_version ||true
+# reinstall haskell stuff
+ghc-pkg-$ghc_version unregister hubris || true
 ghc-$ghc_version --make Setup
 # this is pretty ugly - this line creates the Includes.hs file, 
 # as cabal install ignores the given Setup.hs. FIXME
